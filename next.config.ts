2b26a6e6646f9acdbbafb65bbe4f16f5a417574d
nathyxddd <nathalia.olivarez@alumnos.ucn.cl>
2025-06-03 18:35:30 -0400

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+      protocol: "http",
+      hostname: process.env.DOMAIN || "localhost",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+    ]
+  }
 };
 
 export default nextConfig;
