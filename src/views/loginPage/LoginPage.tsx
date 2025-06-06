e84a94d1ac25f77a492d714f99c9f936e59a96b7
Alexander Rivera <riveraalexander455@gmail.com>
2025-06-05 23:35:37 -0400

    feat: Se agrega la vista del formulario de iniciar sesión

---
+++ src/views/loginPage/LoginPage.tsx
@@ -0,0 +1,101 @@
+"use client";
+import "@fontsource/afacad";
+import { VscAccount } from "react-icons/vsc";
+import z from "zod";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  email: z
+    .string()
+    .email({ message: "Ingrese un correo electrónico válido." })
+    .nonempty({ message: "Email es requerido." }),
+  password: z.string().nonempty({ message: "Contraseña es requerida." }),
+});
+
+export const LoginPage = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      console.log("Valores enviados:", values);
+    } catch (error) {}
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50 px-4 font-[Afacad]">
+      <div className="w-full max-w-md bg-transparent">
+        <h1 className="text-4xl font-bold text-center mb-6">Iniciar sesión</h1>
+
+        {/* Ícono arriba */}
+        <div className="flex justify-center mb-6">
+          <VscAccount className="w-[150px] h-[170px] text-[#2599E7]" />
+        </div>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className = "text-lg">Correo electrónico *</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="correo@example.com"
+                      className="rounded-md shadow-sm"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className = "text-lg">Contraseña *</FormLabel>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="••••••"
+                      className="rounded-md shadow-sm"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button
+              type="submit"
+              className="w-full bg-blue-500 text-white rounded-md shadow hover:bg-blue-600 transition"
+            >
+              INGRESAR
+            </Button>
+          </form>
+        </Form>
+      </div>
+    </div>
+  );
+};
